71d43c7043b167c3cdff878d287bd16aee07bb63
anjakolom <87429737+anjakolom@users.noreply.github.com>
2022-10-19 15:54:24 +0300

    Задание 3. Сценарий логина.

---
+++ selenium_1/src/test/java/TestLogin.java
@@ -0,0 +1,39 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class TestLogin {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        Duration duration = Duration.ofDays(10);
+        wait = new WebDriverWait(driver, duration);
+
+    }
+
+    @Test
+    public void myTest() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+
+    }
+}
