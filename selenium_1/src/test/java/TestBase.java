7d44f3ab8f1138fe407172ea909757078370fd38
anjakolom <87429737+anjakolom@users.noreply.github.com>
2022-10-23 08:55:35 +0300

    Выделен базовый класс TestBase
    Выделен базовый класс TestBase

--- selenium_1/src/test/java/TestBase.java
+++ selenium_1/src/test/java/TestBase.java
@@ -4,28 +4,38 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.time.Duration;
 
 public class TestBase {
 
-    private WebDriver driver;
-    private WebDriverWait wait;
+    public WebDriver driver;
+    public WebDriverWait wait;
 
 
     @Before
     public void start() {
         driver = new ChromeDriver();
-        Duration duration = Duration.ofDays(10);
+
+        /*EdgeOptions options = new EdgeOptions();
+        driver = new EdgeDriver(options);*/
+
+        /*FirefoxOptions caps = new FirefoxOptions();
+        driver = new FirefoxDriver();*/
+
+        /*FirefoxOptions options = new FirefoxOptions();
+        options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Firefox Nightly\\firefox.exe")));
+        driver = new FirefoxDriver();*/
+
+        Duration duration = Duration.ofMillis(10);
         wait = new WebDriverWait(driver, duration);
     }
 
-    @Test
-    public void myTest() {
-        driver.get("http://github.com/anjakolom/selenium/");
-        driver.findElement(By.linkText("Selenium")).click();
-    }
 
     @After
     public void stop() {
--- selenium_1/src/test/java/TestLogin.java
+++ selenium_1/src/test/java/TestLogin.java
@@ -1,48 +1,11 @@
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeDriverService;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.ie.InternetExplorerOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
-import java.time.Duration;
-
-public class TestLogin {
-
-    private WebDriver driver;
-    private WebDriverWait wait;
+public class TestLogin extends TestBase{
 
 
-    @Before
-    public void start() {
-        /*driver = new ChromeDriver();*/
-
-        /*EdgeOptions options = new EdgeOptions();
-        driver = new EdgeDriver(options);*/
-
-        /*FirefoxOptions caps = new FirefoxOptions();
-        driver = new FirefoxDriver();*/
-
-        FirefoxOptions options = new FirefoxOptions();
-        options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Firefox Nightly\\firefox.exe")));
-        driver = new FirefoxDriver();
-        
-        Duration duration = Duration.ofMillis(10);
-        wait = new WebDriverWait(driver, duration);
-    }
 
     @Test
     public void myTest() {
@@ -52,10 +15,5 @@ public class TestLogin {
         driver.findElement(By.name("login")).click();
     }
 
-    @After
-    public void stop() {
-        driver.quit();
-        driver = null;
 
-    }
 }
