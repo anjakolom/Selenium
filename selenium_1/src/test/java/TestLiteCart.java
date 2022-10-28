538b826b48e4bce7162697e191461f0bc70adece
anjakolom <87429737+anjakolom@users.noreply.github.com>
2022-10-28 22:48:20 +0300

    Задание 7.  TestLiteCart

---
+++ selenium_1/src/test/java/TestLiteCart.java
@@ -0,0 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.Collection;
+import java.util.List;
+
+public class TestLiteCart extends TestBase{
+
+    @Test
+    public void stickerTest(){
+
+        driver.get("http://localhost/litecart/en/");
+        if (helper.isElementPresent(driver, By.cssSelector("div.image-wrapper"))) {
+            List<WebElement> ducks = driver.findElements(By.cssSelector("div.image-wrapper"));
+            for (int i=0; i<ducks.size(); i++) {
+                Collection<WebElement> element = ducks.get(i).findElements(By.cssSelector("div.sticker"));
+                Assert.assertEquals(1,element.size());
+            }
+        }
+    }
+
+}
