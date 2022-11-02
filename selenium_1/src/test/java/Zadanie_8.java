83c01841a3de07f89d535b4aec469dbfcaaaa3df
anjakolom <87429737+anjakolom@users.noreply.github.com>
2022-11-02 21:46:47 +0300

    Задание 8. Доработка по замечаниям

--- selenium_1/src/test/java/Zadanie_8.java
+++ selenium_1/src/test/java/Zadanie_8.java
@@ -14,19 +14,20 @@ public class Zadanie_8 extends TestBase {
         driver.get("http://localhost/litecart/admin/login.php");
         helper.login("admin", "admin", driver);
         driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
-        Assert.assertNotNull(driver.findElements(By.cssSelector("tr.row")));
+        Assert.assertTrue(helper.isElementPresent(driver, By.cssSelector("tr.row")));
         List<WebElement> countries = driver.findElements(By.cssSelector("tr.row"));
+
         List<String> arrayCountries = new ArrayList<>();
         List<String> arrayCountries2 = new ArrayList<>();
         for (WebElement c : countries) {
-            Assert.assertNotNull(driver.findElements(By.xpath("td[5]/a")));
+            Assert.assertTrue(helper.isElementPresent(c, By.xpath("td[5]/a")));
             arrayCountries.add(c.findElement(By.xpath("td[5]/a")).getText());
             String zone = c.findElement(By.xpath("td[6]")).getText().trim();
             if (zone.equals("0") == false) {
                 arrayCountries2.add(c.findElement(By.xpath("td[5]/a")).getText());
             }
         }
-        List<String> arrayCountriesExpected = arrayCountries;
+        List<String> arrayCountriesExpected = new ArrayList<String>(arrayCountries);
         Collections.sort(arrayCountriesExpected);
         Assert.assertEquals(arrayCountriesExpected, arrayCountries);
 
@@ -34,19 +35,21 @@ public class Zadanie_8 extends TestBase {
         List<String> arrayCountriesExpected3 = new ArrayList<>();
         for (String c : arrayCountries2) {
             driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
-            Assert.assertNotNull(driver.findElements(By.linkText(c)));
+            Assert.assertTrue(helper.isElementPresent(driver, By.linkText(c)));
             driver.findElement(By.linkText(c)).click();
-            Assert.assertNotNull(driver.findElements(By.cssSelector("#table-zones td:nth-child(3) input[type=hidden]")));
-            List<WebElement> countries3 = driver.findElements(By.cssSelector("#table-zones td:nth-child(3) input[type=hidden]"));
+            Assert.assertTrue(helper.isElementPresent(driver, By.cssSelector("#table-zones td:nth-child(3)")));
+            List<WebElement> countries3 = driver.findElements(By.cssSelector("#table-zones td:nth-child(3)"));
             for (WebElement s : countries3) {
-                arrayCountries3.add(s.getAttribute("defaultValue"));
+                if (s.getAttribute("innerText").length()>0 && s.isDisplayed()){
+                    arrayCountries3.add(s.getAttribute("innerText"));
+                }
             }
-            arrayCountriesExpected3 = arrayCountries3;
+            arrayCountriesExpected3 = new ArrayList<String>(arrayCountries3);
             Collections.sort(arrayCountriesExpected3);
             Assert.assertEquals(arrayCountriesExpected3, arrayCountries3);
+            arrayCountries3.clear();
+            arrayCountriesExpected3.clear();
 
         }
-
-
     }
 }
