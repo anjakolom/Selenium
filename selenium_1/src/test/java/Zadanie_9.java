5a37e66ce25ee44ee809d04a375bb681934a4798
anjakolom <anjakolom@gmail.com>
2022-11-05 15:49:34 +0300

    Задание 9. Доработано

--- selenium_1/src/test/java/Zadanie_9.java
+++ selenium_1/src/test/java/Zadanie_9.java
@@ -16,29 +16,37 @@ public class Zadanie_9 extends TestBase {
         helper.login("admin", "admin", driver);
         driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
 
-        //#content > form > table  td:nth-child(3) > a
         Assert.assertTrue(helper.isElementPresent(driver,By.cssSelector("#content > form > table  td:nth-child(3) > a")));
-        List<WebElement> countries = driver.findElements(By.cssSelector("#content > form > table  td:nth-child(3) > a"));
-        for (WebElement c : countries) {
+        List<WebElement> line = driver.findElements(By.cssSelector("#content > form > table  td:nth-child(3) > a"));
+        List<String> countries = new ArrayList<>();
+
+        for (WebElement c : line) {
             System.out.println(c.getAttribute("innerText"));
-           // c.findElement(By.linkText(c.getAttribute("innerText"))).click();
+            countries.add(c.getAttribute("innerText").trim());
         }
 
-        Assert.assertTrue(helper.isElementPresent(driver,By.cssSelector("#content > h1")));
-        Assert.assertEquals(driver.findElement(By.cssSelector("#content > h1")).getText(), "Edit Geo Zone");
+        for (int i=0; i<countries.size();i++){
+            Assert.assertTrue(helper.isElementPresent(driver,By.linkText(countries.get(i))));
+            driver.findElement(By.linkText(countries.get(i))).click();
+            Assert.assertTrue(helper.isElementPresent(driver, By.cssSelector("#content > h1")));
+            Assert.assertEquals(driver.findElement(By.cssSelector("#content > h1")).getText(), "Edit Geo Zone");
+            Assert.assertTrue(helper.isElementPresent(driver,By.cssSelector("input[name=\"name\"]")));
+            Assert.assertEquals(countries.get(i),driver.findElement(By.cssSelector("input[name=\"name\"]")).getAttribute("defaultValue"));
+            Assert.assertTrue(helper.isElementPresent(driver, By.cssSelector("#table-zones")));
+            Assert.assertTrue(helper.isElementPresent(driver, By.cssSelector("#table-zones td:nth-child(3) [selected=\"selected\"]")));
+            List<WebElement> zones = driver.findElements(By.cssSelector("#table-zones td:nth-child(3) [selected=\"selected\"]"));
+            List<String> arrayZonesExpected = new ArrayList<String>();
+            List<String> arrayZones = new ArrayList<String>();
 
-        Assert.assertTrue(helper.isElementPresent(driver, By.cssSelector("#table-zones")));
-        Assert.assertTrue(helper.isElementPresent(driver, By.cssSelector("#table-zones td:nth-child(3) [selected=\"selected\"]")));
-        List<WebElement> zones = driver.findElements(By.cssSelector("#table-zones td:nth-child(3) [selected=\"selected\"]"));
-        List<String> arrayZonesExpected = new ArrayList<String>();
-        List<String> arrayZones = new ArrayList<String>();
-        for (WebElement c : zones) {
-            System.out.println(c.getAttribute("innerText"));
-            arrayZones.add(c.getAttribute("innerText"));
+            for (WebElement z: zones) {
+                System.out.println(z.getAttribute("innerText"));
+                arrayZones.add(z.getAttribute("innerText"));
+            }
+            arrayZonesExpected = new ArrayList<String>(arrayZones);
+            Collections.sort(arrayZonesExpected);
+            Assert.assertEquals(arrayZonesExpected, arrayZones);
+
+            driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
         }
-        arrayZonesExpected = new ArrayList<String>(arrayZones);
-        Collections.sort(arrayZonesExpected);
-        Assert.assertEquals(arrayZonesExpected, arrayZones);
-
     }
 }
